c339df2d2d5924b48d96eaf460fc8acff2624731
RafaelAcheSales <rafael.ache@hotmail.com>
2021-08-04 14:12:21 -0300

    first vanilla commit

--- main/ethernet_example_main.c
+++ main/ethernet_example_main.c
@@ -18,12 +18,10 @@
 #include "sdkconfig.h"
 #if CONFIG_ETH_USE_SPI_ETHERNET
 #include "driver/spi_master.h"
-#include "gpio17.h"
-
 #endif // CONFIG_ETH_USE_SPI_ETHERNET
 
 static const char *TAG = "eth_example";
-const int use_dhcp = 0;
+
 /** Event handler for Ethernet events */
 static void eth_event_handler(void *arg, esp_event_base_t event_base,
                               int32_t event_id, void *event_data)
@@ -31,7 +29,7 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base,
     uint8_t mac_addr[6] = {0};
     /* we can get the ethernet driver handle from event data */
     esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
-    //ip4_addr_t current_ip;
+
     switch (event_id) {
     case ETHERNET_EVENT_CONNECTED:
         esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
@@ -70,8 +68,6 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
 
 void app_main(void)
 {
-    set_pin_17(1);
-    vTaskDelay(100);
     // Initialize TCP/IP network interface (should be called only once in application)
     ESP_ERROR_CHECK(esp_netif_init());
     // Create default event loop that running in background
@@ -81,7 +77,7 @@ void app_main(void)
     // Set default handlers to process TCP/IP stuffs
     ESP_ERROR_CHECK(esp_eth_set_default_handlers(eth_netif));
     // Register user defined event handers
-    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, eth_netif));
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
 
     eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
@@ -177,22 +173,4 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)));
     /* start Ethernet driver state machine */
     ESP_ERROR_CHECK(esp_eth_start(eth_handle));
-    if (use_dhcp == 1) {
-        esp_netif_dhcpc_start(eth_netif);
-    } else {
-        //not dhcp
-        ESP_ERROR_CHECK(esp_netif_dhcpc_stop(eth_netif));
-        vTaskDelay(500);
-        char* ip = "10.0.0.167";
-        char* gateway = "10.0.0.1";
-        char* netmask = "255.255.255.0";
-        esp_netif_ip_info_t info_t;
-        memset(&info_t, 0, sizeof(esp_netif_ip_info_t));
-        ip4addr_aton((const char *)ip, (ip4_addr_t *) &info_t.ip.addr);
-        ip4addr_aton((const char *)gateway,(ip4_addr_t *) &info_t.gw.addr);
-        ip4addr_aton((const char *)netmask,(ip4_addr_t *) &info_t.netmask.addr);
-        esp_netif_set_ip_info(eth_netif, &info_t);
-    }
-
-    
 }
